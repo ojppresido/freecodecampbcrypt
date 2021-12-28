4fb81381f93a3e1185084a8f788180de89b72812
ojppresido <you@ojppresido@gmail.com>
2021-12-28 19:49:22 +0100

    test3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
-const bcrypt = require('bcrypt');
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
